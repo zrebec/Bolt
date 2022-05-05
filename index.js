8d18f6a1bc58c234691c88a26647bcabf028b941
Martin Bencik <martin.bencik@ibm.com>
2022-05-05 06:20:00 +0200

    Basic template for every basic file

---
+++ index.js
@@ -0,0 +1,10 @@
+// Variables
+const appName = 'Bolt'
+
+// Event listeners
+window.addEventListener('load', onload)
+
+// Functions
+function onload() {
+  console.log(`${appName} was sucessfully loaded`)
+}
